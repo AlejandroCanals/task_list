b39bce575fd0a83c672862f74ea2843d44da09cf
iTzSRK <Alejandrocanals17@gmail.com>
2024-07-04 17:58:07 +0200

    Nueva vista para actualizar tareas

--- task_list/app/views.py
+++ task_list/app/views.py
@@ -19,6 +19,22 @@ def add_task(request):
         form = TaskForm()
     return render(request,'app/add_task.html',{'form': form})
 
+
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'app/update_task.html', {'form': form})
+
+
+        
+
 def delete_task(request,task_id):
     task = get_object_or_404(Task, id=task_id)
     
@@ -28,4 +44,4 @@ def delete_task(request,task_id):
         task.delete()
         return redirect('task_list')
     
-    return redirect('task_list')  
+    return render(request, 'app/delete_task.html', {'task': task})
